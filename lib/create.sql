22aac4e114a690acd4c1a0dc68f503af46584d7d
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-26 20:44:57 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students
+    (id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO students (name)
+    VALUES ("Mr. Chocolate");
+INSERT INTO students (name)
+    VALUES ("Rowdy");
+INSERT INTO students (name)
+    VALUES ("Tabitha");
+INSERT INTO students (name)
+    VALUES ("Sergeant Brown");
+INSERT INTO students (name)
+    VALUES ("Melissa");
+INSERT INTO students (name)
+    VALUES ("Grinch");
+INSERT INTO students (name)
+    VALUES ("Wendy");
+INSERT INTO students (name)
+    VALUES ("Wendy");
